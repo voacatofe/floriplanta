e332d03fde2f5892636d9c5d389668b19c394508
Developer <user@example.com>
2025-05-05 01:32:20 -0300

    fix: corrige importações em página de pesquisas

--- app/cannabis/pesquisas/page.tsx
+++ app/cannabis/pesquisas/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import Header from "../../components/Header";r';
-import Footer from '../../../../components/Footer';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import { FlaskConical, BookOpen, Search, Database, Link as LinkIcon } from 'lucide-react'; // Example icons
 
 // Reusable Sidebar Component
