236a2b759ccf532a92e0b2175380a0c08b56f153
Seu Nome <seu_email@exemplo.com>
2025-06-18 19:09:54 -0300

    deploy

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -25,7 +25,7 @@ export default function AdminLoginPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen">
       <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle className="text-2xl font-bold text-center">Login do Administrador</CardTitle>
